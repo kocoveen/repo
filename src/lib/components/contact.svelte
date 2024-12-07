c4ecc1096385f23ad1b27d31072455453ea23f21
kocoveen <kocoveen@gmail.com>
2024-12-07 16:12:24 +0000

    feat: add title in map of contact page

--- src/lib/components/contact.svelte
+++ src/lib/components/contact.svelte
@@ -10,6 +10,7 @@
 	<section class="mapbox" data-mapbox>
 		<figure>
 			<iframe
+				title="mapbox"
 				src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6331.747987661466!2d126.93905246459337!3d37.48729977834594!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357c9f9087101355%3A0x47c13fc1e48a2473!2z7ISc7Jq47Yq567OE7IucIOq0gOyVheq1rCDsnYDsspzrj5k!5e0!3m2!1sko!2skr!4v1733053657351!5m2!1sko!2skr"
 				width="400"
 				height="300"
