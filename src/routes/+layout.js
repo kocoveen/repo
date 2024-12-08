5e75c77d015e287444d46ea10014de9f94513225
kocoveen <kocoveen@gmail.com>
2024-12-08 13:01:43 +0000

    feat: when landing, redirect about

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,1 +1,12 @@
 export const prerender = true;
+
+import { redirect } from '@sveltejs/kit';
+
+export function load({ url }) {
+  const { pathname } = url;
+
+  // 루트 경로로 접근했을 때 리다이렉트
+  if (pathname === '/') {
+    throw redirect(302, '/about'); // {base}/about으로 리다이렉트
+  }
+}
