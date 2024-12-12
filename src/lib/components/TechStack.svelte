e000921642d951aab12777841e09eb13332a0d8b
kocoveen <kocoveen@gmail.com>
2024-12-12 08:34:40 +0000

    feat: make techstack component

---
+++ src/lib/components/TechStack.svelte
@@ -0,0 +1,15 @@
+<script>
+	let { data, desc } = $props();
+</script>
+
+<li class="tech-item">
+	<div class="tech-icon-box">
+		<img src={data.icon} alt="tech stack" width="30" />
+	</div>
+
+	<div class="tech-info">
+		<p class="tech-title">{data.name}</p>
+
+		<desc>{desc}</desc>
+	</div>
+</li>
---
+++ src/lib/data/techStack.js
@@ -0,0 +1,41 @@
+export const data = {
+    "java": {
+        "name": "Java",
+        "icon": "https://noticon-static.tammolo.com/dgggcrkxq/image/upload/v1667082075/noticon/evii0kwjuapddhzjmdrb.png"
+    },
+
+    "spring boot": {
+        "name": "Spring Boot",
+        "icon": "https://noticon-static.tammolo.com/dgggcrkxq/image/upload/v1583139980/noticon/vtzecmjzn39cifnjtonx.png"
+    },
+
+    "spring security": {
+        "name": "Spring Security",
+        "icon": "https://noticon-static.tammolo.com/dgggcrkxq/image/upload/v1686935854/noticon/r7w1ipwmdmhlfzqfw69h.png"
+    },
+
+    "jpa": {
+        "name": "JPA",
+        "icon": "https://noticon-static.tammolo.com/dgggcrkxq/image/upload/v1687307488/noticon/o9lxyva5z8zbwyeaxers.png"
+    },
+
+    "mysql": {
+        "name": "MySQL",
+        "icon": "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg"
+    },
+
+    "kafka": {
+        "name": "Kafka",
+        "icon": "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/apachekafka/apachekafka-original.svg"
+    },
+
+    "jwt": {
+        "name": "JWT",
+        "icon": "https://noticon-static.tammolo.com/dgggcrkxq/image/upload/v1569654347/noticon/mdujedvj9w8c9rz9phny.png"
+    },
+
+    "fcm": {
+        "name": "FCM",
+        "icon": "https://noticon-static.tammolo.com/dgggcrkxq/image/upload/v1566913958/noticon/uoqjdixts4lwsgtsa1pd.png"
+    },
+}
